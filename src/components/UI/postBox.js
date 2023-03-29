717741912be89b79f612336358ed657e10d6870f
DESKTOP-IQ624AA\yk_Ha <proguy425@gmail.com>
2023-03-29 18:06:45 +0900

    6일차 post item component

--- src/components/UI/postBox.js
+++ src/components/UI/postBox.js
@@ -1,3 +1,15 @@
-export default function PostBox() {
-    return <div>박스컴포넌트</div>
+import styles from './postBox.module.scss';
+
+export default function PostBox(props) {
+    const { title, date, postImg } = props;
+
+    return (
+        <>
+            <li className={styles.postItem}>
+                <img src={postImg && '/images/defaultImg.png'} alt="블로그이미지" />
+                <div>{title}</div>
+                <div>{date}</div>
+            </li>
+        </>
+    );
 }
