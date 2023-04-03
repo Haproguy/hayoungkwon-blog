4965e01a3ebde609849a402521caf4b68e0fbbaf
DESKTOP-IQ624AA\yk_Ha <proguy425@gmail.com>
2023-04-03 17:58:16 +0900

    9일차 react-quill을 이용한 post기능 사진첨부기능 추가 예정

--- src/pages/posting/createpost.js
+++ src/pages/posting/createpost.js
@@ -1,18 +1,61 @@
-import { useState, useRef } from "react";
 import axios from 'axios';
+import dynamic from 'next/dynamic';
+import { useCallback, useState, useMemo, useRef } from "react";
 import Button from "@/components/UI/button";
+const BoardEdit = dynamic(() => import('@/components/board/boardEditor'), {
+    ssr: false
+})
 
+import styles from './createPost.module.scss';
 
 export default function WritePage() {
-    const quill = document.querySelector('.ql-editor');
-    const images = quill.querySelectorAll('img');
-    const [editHtml , setEditHtml] = useState('');
+    const titleRef = useRef();
+    const boardRef = useRef('');
+    const postDate = new Intl.DateTimeFormat('kr').format(new Date());
+
+    const [boardContents, setBoardContents] = useState('');
+
+    const quillChangeHandler = useCallback((contents) => {
+        setBoardContents(contents);
+        console.log(typeof (boardContents));
+    }, []);
+
+    const boardClickHandler = () => {
+        axios.post('/api/posting/createpost', {
+            title: titleRef.current.value,
+            content: boardContents,
+            postdate: postDate
+        })
+            .then((res) => {
+                console.log(res.data);
+            })
+    }
 
 
     return (
-        <>
+        <div className={styles.createPost}>
             <h1>글작성</h1>
-            <Button />
-        </>
+
+            <div className={styles.titleWrap}>
+                <input
+                    type="text"
+                    placeholder='제목을 입력해주세요'
+                    minLength={1}
+                    maxLength={26}
+                    ref={titleRef} />
+
+                <Button
+                    btnText='작성완료'
+                    btnClickHandler={boardClickHandler} />
+            </div>
+            <div className={styles.contentsWrap}>
+                <BoardEdit
+                    quillRef={boardRef}
+                    quillChangeHandler={quillChangeHandler}
+                    quillContents={boardContents}
+                />
+            </div>
+
+        </div>
     );
 }
