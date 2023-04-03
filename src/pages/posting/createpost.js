61155917974b026b87ce4fb6f838b6d9bdd7e8e3
DESKTOP-IQ624AA\yk_Ha <proguy425@gmail.com>
2023-04-03 13:52:28 +0900

    9일차 react-quill도입

--- src/pages/posting/createpost.js
+++ src/pages/posting/createpost.js
@@ -1,22 +1,14 @@
 import { useState, useRef } from "react";
 import axios from 'axios';
-import Write from "./write,";
 import Button from "@/components/UI/button";
 
 
 export default function WritePage() {
-    const quill = document.querySelector('.ql-editor');
-    const images = quill.querySelectorAll('img');
-    const [editHtml , setEditHtml] = useState('');
 
-    const onChangeValueHander = ()=>{
-        setEditHtml()
-    }
 
     return (
         <>
             <h1>글작성</h1>
-            <Write />
             <Button />
         </>
     );
